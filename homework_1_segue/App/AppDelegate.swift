f700d083a704259842146a0759b632cece041e8f
Panviktor <panafviktor@gmail.com>
2019-06-23 22:10:56 +0300

    init commit

---
+++ homework_1_segue/App/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  homework_1_segue
+//
+//  Created by Виктор on 23/06/2019.
+//  Copyright © 2019 SwiftViktor. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+}
+
