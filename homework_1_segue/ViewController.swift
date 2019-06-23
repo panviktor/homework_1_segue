74af75c3f5d10313a38f305a3f26644909ebf755
Panviktor <panafviktor@gmail.com>
2019-06-23 22:05:22 +0300

    Initial Commit

---
+++ homework_1_segue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  homework_1_segue
+//
+//  Created by Виктор on 23/06/2019.
+//  Copyright © 2019 SwiftViktor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
